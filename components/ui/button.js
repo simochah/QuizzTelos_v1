636edd0816628517dac7ba93c1db0d5aa91e3680
simochah <contact@simochah.com>
2025-03-18 16:10:38 +0000

    Update button.js

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,7 +1,9 @@
-export function Button({ children, onClick }) {
+// button.js
+export function Button({ children, onClick, className = "" }) {
   return (
     <button
-      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+      // On concatène ici nos classes par défaut + celles passées en props
+      className={`px-4 py-2 text-white rounded hover:bg-blue-600 ${className}`}
       onClick={onClick}
     >
       {children}
